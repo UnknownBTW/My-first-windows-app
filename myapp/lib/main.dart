b31badbcdc57702746fce50dd5387f22d72aa61f
UnknownBTW <64477446+UnknownBTW@users.noreply.github.com>
2024-06-24 22:23:06 +0100

    Bottom navigation bar
    bottom navigation bar

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -12,19 +12,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(    
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.purple,
           title: const Text("My new app using"),
         ),
 
-        body: Center(
-          child: const Padding(
-            child: const Text("This is looking nice so far"),
-            padding: const EdgeInsets.all(10),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            print('Pressed!!');
+          },
+        ),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-        )
+             BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'business',
+            ),
+             BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
+
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            ),
+            Align(
+              child: Icon(Icons.verified),
+              alignment: Alignment.center,
+            ),
+          ],
+        ),
       )
     );
   }
